e198c9e8ef5c2a996b8df4e28e332a6eed534a31
we-as <smokedetector59@gmail.com>
2024-06-15 17:59:01 +0530

    new page start match

---
+++ lib/sports_app.dart
@@ -0,0 +1,177 @@
+import 'package:flutter/material.dart';
+import 'start_match.dart'; // Import your Start Match page
+// import 'go_live.dart'; // Import your Go Live page
+// import 'looking_for.dart'; // Import your Looking For page
+// import 'leaderboard.dart'; // Import your Leaderboard page
+// import 'news.dart'; // Import your News page
+// import 'tournament.dart'; // Import your Tournament page
+
+// Entry point of the application
+void main() => runApp(const MyApp());
+
+// Main widget of the application
+class MyApp extends StatelessWidget {
+  final appTitle = 'SportsApp';
+
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: appTitle,
+      debugShowCheckedModeBanner: false, // Set this property to false
+      home: SportsApp(title: appTitle),
+    );
+  }
+}
+
+// Stateful widget to manage state for the bottom navigation bar
+class SportsApp extends StatefulWidget {
+  final String title;
+
+  const SportsApp({Key? key, required this.title}) : super(key: key);
+
+  @override
+  _SportsAppState createState() => _SportsAppState();
+}
+
+class _SportsAppState extends State<SportsApp> {
+  // Index of the currently selected bottom navigation bar item
+  int _selectedIndex = 0;
+
+  // List of widgets to display based on the selected index
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text('Home'),
+    Text('Football'),
+    Text('Hockey'),
+    Text('Volleyball'),
+  ];
+
+  // Method to handle bottom navigation bar item taps
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        // Display the widget corresponding to the selected index
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      // Side navigation drawer
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const UserAccountsDrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              accountName: Text(
+                "User Login Name",
+                style: TextStyle(fontSize: 18),
+              ),
+              accountEmail: Text("userloginemail@gmail.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Text(
+                  "U",
+                  style: TextStyle(fontSize: 30.0, color: Colors.blue),
+                ),
+              ),
+            ),
+            // Drawer menu items with relevant icons and navigation logic
+            _createDrawerItem(
+                icon: Icons.sports_tennis,
+                text: 'Start Match',
+                onTap: () => _navigateTo(context, StartMatchPage())),
+            // _createDrawerItem(
+            //     icon: Icons.live_tv,
+            //     text: 'Go Live',
+            //     onTap: () => _navigateTo(context, GoLivePage())),
+            // _createDrawerItem(
+            //     icon: Icons.search,
+            //     text: 'Looking For',
+            //     onTap: () => _navigateTo(context, LookingForPage())),
+            // _createDrawerItem(
+            //     icon: Icons.leaderboard,
+            //     text: 'Leaderboard',
+            //     onTap: () => _navigateTo(context, LeaderboardPage())),
+            // _createDrawerItem(
+            //     icon: Icons.article,
+            //     text: 'News',
+            //     onTap: () => _navigateTo(context, NewsPage())),
+            // _createDrawerItem(
+            //     icon: Icons.emoji_events,
+            //     text: 'Tournament',
+            //     onTap: () =>
+            //         _navigateTo(context, TournamentPage())), // Updated icon
+            _createDrawerItem(
+                icon: Icons.logout,
+                text: 'LogOut',
+                onTap: () {
+                  // Handle logout logic here
+                  Navigator.pop(context); // Close the drawer on item tap
+                }),
+          ],
+        ),
+      ),
+      // Bottom navigation bar with four items
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(
+          canvasColor: Colors.blue, // Set the background color here
+        ),
+        child: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.sports_soccer),
+              label: 'Football',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.sports_hockey),
+              label: 'Hockey',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.sports_volleyball),
+              label: 'Volleyball',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Color.fromARGB(255, 6, 19, 30),
+          onTap: _onItemTapped,
+        ),
+      ),
+    );
+  }
+
+  // Helper method to create drawer items with navigation logic
+  ListTile _createDrawerItem(
+      {required IconData icon,
+      required String text,
+      required Function() onTap}) {
+    return ListTile(
+      leading: Icon(icon),
+      title: Text(text),
+      onTap: () {
+        Navigator.pop(context); // Close the drawer on item tap
+        onTap();
+      },
+    );
+  }
+
+  // Helper method to navigate to a new page
+  void _navigateTo(BuildContext context, Widget page) {
+    Navigator.push(context, MaterialPageRoute(builder: (context) => page));
+  }
+}
---
+++ lib/team_details_dialog.dart
@@ -0,0 +1,144 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class TeamDetailsDialog extends StatefulWidget {
+  final String team;
+
+  const TeamDetailsDialog({required this.team});
+
+  @override
+  _TeamDetailsDialogState createState() => _TeamDetailsDialogState();
+}
+
+class _TeamDetailsDialogState extends State<TeamDetailsDialog> {
+  final _formKey = GlobalKey<FormState>();
+  String _teamName = '';
+  int _numPlayers = 5;
+  PickedFile? _teamLogo;
+  final List<TextEditingController> _playerControllers = [];
+
+  final ImagePicker _picker = ImagePicker();
+
+  @override
+  void initState() {
+    super.initState();
+    _initializePlayerControllers();
+  }
+
+  void _initializePlayerControllers() {
+    _playerControllers.clear();
+    for (int i = 0; i < _numPlayers; i++) {
+      _playerControllers.add(TextEditingController());
+    }
+  }
+
+  @override
+  void dispose() {
+    for (var controller in _playerControllers) {
+      controller.dispose();
+    }
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text('Enter Details for ${widget.team}'),
+      content: Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              GestureDetector(
+                onTap: () async {
+                  final pickedFile =
+                      await _picker.getImage(source: ImageSource.gallery);
+                  setState(() {
+                    _teamLogo = pickedFile;
+                  });
+                },
+                child: _teamLogo == null
+                    ? CircleAvatar(
+                        radius: 40,
+                        child: Icon(Icons.add_a_photo),
+                      )
+                    : CircleAvatar(
+                        radius: 40,
+                        backgroundImage: FileImage(File(_teamLogo!.path)),
+                      ),
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Team Name'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a team name';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _teamName = value!;
+                },
+              ),
+              DropdownButtonFormField<int>(
+                value: _numPlayers,
+                decoration: InputDecoration(labelText: 'Number of Players'),
+                items: [5, 7, 11].map((int value) {
+                  return DropdownMenuItem<int>(
+                    value: value,
+                    child: Text(value.toString()),
+                  );
+                }).toList(),
+                onChanged: (value) {
+                  setState(() {
+                    _numPlayers = value!;
+                    _initializePlayerControllers();
+                  });
+                },
+              ),
+              ..._playerControllers.map((controller) {
+                int index = _playerControllers.indexOf(controller) + 1;
+                return TextFormField(
+                  controller: controller,
+                  decoration: InputDecoration(labelText: 'Player $index Name'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a name for Player $index';
+                    }
+                    return null;
+                  },
+                );
+              }).toList(),
+            ],
+          ),
+        ),
+      ),
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: Text('Cancel'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            if (_formKey.currentState!.validate()) {
+              _formKey.currentState!.save();
+              List<String> playerNames =
+                  _playerControllers.map((c) => c.text).toList();
+              Navigator.of(context).pop({
+                'teamName': _teamName,
+                'numPlayers': _numPlayers,
+                'teamLogo': _teamLogo?.path,
+                'players': playerNames,
+              });
+            }
+          },
+          child: Text('Submit'),
+        ),
+      ],
+    );
+  }
+}
