1b2a93bce615de8efc286ac7d1d5754a1aaf2df4
backbenchers <backbenchers326@gmail.com>
2024-07-06 17:57:29 +0530

    Added the dockerfile

--- API/Match Management/POST/NewMatch.js
+++ API/Match Management/POST/NewMatch.js
@@ -1,50 +1,50 @@
 const express = require('express');
-const { OpenConnection, CloseConnection } = require('C:/Users/mersa/Desktop/Back_end/SportsApplication/DBManager/Connection.js');
+const { OpenConnection, CloseConnection } = require('../../../DBManager/Connection.js');
+const Modules = require('../../../Modules/Common.js')
 const logger = require('../../../log');
 
 const router = express.Router();
 
 router.post('/NewMatch', async (req, res) => {
 
-  const {end_time,match_id,result,score,start_time,status,team_a,team_b,tournament_id,venue} = req.body;
-
-  // Define an array of required parameters
-  const requiredParams = ['end_time', 'match_id', 'result', 'score', 'start_time', 'status', 'team_a', 'team_b', 'tournament_id', 'venue'];
-
-  // Check if any required parameter is missing in req.body
-  const missingParams = requiredParams.filter(param => !(param in req.body));
-
-  if (missingParams.length > 0) {
-    // Throw an error indicating missing parameters
-    const missingParamsList = missingParams.join(', ');
-    const errorMessage = `Missing ${missingParams.length} required parameter(s): ${missingParamsList}`;
-    logger.error(errorMessage);
-    return res.status(400).json({ error: errorMessage });
-  }
-
-  const { 'Team A': teamAScore, 'Team B': teamBScore } = score;  // Nested destructuring with renaming
-
-  const db = OpenConnection();
-
   try {
-    await db.collection('matches').doc(match_id.toString()).set({
-      end_time,
-      match_id,
-      result,
-      score: { team_a: teamAScore, team_b: teamBScore },  // Store the nested object correctly
-      start_time,
-      status,
-      team_a,
-      team_b,
-      tournament_id,
-      venue
-    });
-    res.status(200).json({ message: 'Match details stored successfully' });
-  } catch (err) {
+    const { logo, team, user_id, venue, match_status, match_type, start_time, duration ,tournament_id = null } = req.body;
+
+    const match_id = Modules.generateUniqueId()
+    const timestamp = Modules.generatetimestamp()
+
+    // Define an array of required parameters
+    const requiredParams = [ 'logo', 'user_id', 'venue', 'match_status', 'match_type', 'start_time', 'duration'];
+
+    // Check if any required parameter is missing in req.body
+    const missingParams = requiredParams.filter(param => !(param in req.body));
+
+    if (missingParams.length > 0) {
+      // Throw an error indicating missing parameters
+      const missingParamsList = missingParams.join(', '); // Returns a new string by concatenating all the elements in the array
+      const errorMessage = `Missing ${missingParams.length} required parameter(s): ${missingParamsList}`;
+      logger.error(errorMessage);
+      return res.status(400).json({ error: errorMessage });
+    }
+
+
+    const db = OpenConnection();
+
+    try {
+      await db.collection('matches').doc("Match" + match_id.toString()).set({
+        match_id,logo,team, user_id,venue,match_status,match_type,start_time,duration,tournament_id,timestamp
+      });
+      res.status(200).json({ message: 'Match details stored successfully' , Id : match_id});
+    } catch (err) {
+      logger.error(err)
+      res.status(500).json({ error: 'Failed to store match details' });
+    } finally {
+      CloseConnection();
+    }
+  }
+  catch (err) {
     logger.error(err)
-    res.status(500).json({ error: 'Failed to store match details' });
-  } finally {
-    CloseConnection();
+    res.status(500).json({ error: 'Failed to store match details see the log file for more details' });
   }
 });
 
