0b7e9b808125d14c3c2ba458a773aa32c2c479bd
backbenchers326 <backbenchers326@gmail.com>
2024-08-03 10:56:55 +0530

    Updated the Api

--- BackEnd/SportsApplication/API/Match Management/POST/NewMatch.js
+++ BackEnd/SportsApplication/API/Match Management/POST/NewMatch.js
@@ -8,13 +8,16 @@ const router = express.Router();
 router.post('/NewMatch', async (req, res) => {
 
   try {
-    const { logo, team, user_id, venue, match_status, match_type, start_time, duration ,tournament_id = null } = req.body;
+    const {team, user_id, venue, match_status, match_type, start_time, duration ,tournament_id = null } = req.body;
 
     const match_id = Modules.generateUniqueId()
     const timestamp = Modules.generatetimestamp()
-
+    team[0].teamid=Modules.generateUniqueId()
+    team[1].teamid=Modules.generateUniqueId()
+    team[0].finalscore="0"
+    team[1].finalscore="0"
     // Define an array of required parameters
-    const requiredParams = [ 'logo', 'user_id', 'venue', 'match_status', 'match_type', 'start_time', 'duration'];
+    const requiredParams = ['user_id', 'venue', 'match_status', 'match_type', 'start_time', 'duration'];
 
     // Check if any required parameter is missing in req.body
     const missingParams = requiredParams.filter(param => !(param in req.body));
@@ -31,7 +34,7 @@ router.post('/NewMatch', async (req, res) => {
     const db = OpenConnection();
     try {
       await db.collection('matches').doc("Match" + match_id.toString()).set({
-        match_id,logo,team, user_id,venue,match_status,match_type,start_time,duration,tournament_id,timestamp
+        match_id,team, user_id,venue,match_status,match_type,start_time,duration,tournament_id,timestamp
       });
       res.status(200).json({ message: 'Match details stored successfully' , Id : match_id});
     } catch (err) {
