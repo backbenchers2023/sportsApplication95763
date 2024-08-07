51d841ec2507273030aa67165da3329d979e8929
backbenchers <backbenchers326@gmail.com>
2024-08-07 17:34:04 +0000

    Added the Cors

--- BackEnd/SportsApplication/index.js
+++ BackEnd/SportsApplication/index.js
@@ -1,23 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const matchRoutes = require('./API/Match Management/POST/NewMatch')
+const cors = require('cors');
+const matchRoutes = require('./API/Match Management/POST/NewMatch');
 const startWebSocket = require('./API/LiveScore/Commentry/Commentry');
 const http = require('http');
 
-const app = express()
+const app = express();
 const port = 3000;
+
+// Use CORS middleware
+app.use(cors());
 
 app.use(bodyParser.json());
 
-//Roting 
+// Routing
 app.use('/match', matchRoutes);
-app.listen(port,'0.0.0.0', () => {
-    console.log(`Server is running on port ${port}`);
-})
 
-//Websocket
+// Create HTTP server
 const server = http.createServer(app);
-server.listen(3050,'0.0.0.0',() => {
+
+// WebSocket
+server.listen(3050, '0.0.0.0', () => {
     console.log(`Server is running on port 3050`);
     startWebSocket(server); // Start WebSocket on the same server
+});
+
+// Listen for HTTP requests
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server is running on port ${port}`);
 });
